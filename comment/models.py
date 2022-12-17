ae355e28bcdb696adae3d391ac7bd95e165ba6c7
Sachin Sharma <105771317+sachinsharmaofficial@users.noreply.github.com>
2022-12-17 22:31:37 +0530

    Add files via upload

---
+++ comment/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+from post.models import Post
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True, null=True)
+
+ 
